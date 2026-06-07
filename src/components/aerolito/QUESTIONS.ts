479c35428f721608b22d881edecfe2f3716a8611
Guilherme Resende <guiresende20@gmail.com>
2026-06-07 20:40:04 -0300

    feat(aerolito): definir as 5 perguntas como constante

---
+++ src/components/aerolito/QUESTIONS.ts
@@ -0,0 +1,9 @@
+export const AEROLITO_QUESTIONS = [
+  "Que características vocês acham mais importantes que eu desenvolva / fortaleça na minha função aqui na Aeroli.to?",
+  "Que tipo de apoio vocês esperam de mim quando estiverem desenvolvendo projetos com clientes?",
+  "Posso trazer lanches comunitários? Se sim, quais?",
+  "Como vocês imaginam que eu posso contribuir (métrica de êxito) nos 6 primeiros meses?",
+  "O que vocês não querem que eu faça? (anti-objetivos)",
+] as const;
+
+export const AEROLITO_QUESTION_COUNT = AEROLITO_QUESTIONS.length;
